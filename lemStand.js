b55fb654d2ed135901066241fc2c96eb497960da
yaelschlotman <koshercouponmom@gmail.com>
2016-01-11 09:26:11 -0600

    initial commi

---
+++ lemStand.js
@@ -0,0 +1,8 @@
+"use strict";
+//criteria for game:
+//wholesale w/variable prices
+//weather system
+//way to predict weather
+//weather affects sales
+//player decides price/how many to make each day
+//price affects sales
